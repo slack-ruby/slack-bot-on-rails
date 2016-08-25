3bf547f7ed0295d1919fe48b199f7dd62b06a116
chao-xian <dev@kelvingan.co.uk>
2016-08-25 20:01:14 +0100

    Add Heroku deploy button

---
+++ app.js
@@ -0,0 +1,12 @@
+{
+  "name": "Slack-Bot-On-Rails",
+  "description": "A slack bot that responds to `say`, running on Rails with a React front-end that displays messages.",
+  "repository": "https://github.com/slack-ruby/slack-bot-on-rails",
+  "keywords": ["slack", "rails", "bot"],
+  "env": {
+    "SLACK_API_TOKEN": {
+      description: "You Slack API token",
+      value: "UPDATE_WITH_YOUR_TOKEN"
+    }
+  }
+}
